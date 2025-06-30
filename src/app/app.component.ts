79cd0946dd3cda0f74fe5166c230ef9485b1928b
david-vargas98 <edgar981125@live.com.mx>
2025-06-29 19:28:47 -0600

    feat: ngAfterViewInit hook.
    Some exercises where we use the ngAfterViewInit to try to access courseImage which will throw an error since the courseImage is an element from a child component of the current app.component, and also throws an error since the are trying to change a descripion property when it has been already defined and suddenly we changed it a few moments after.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import {COURSES} from '../db-data';
 import { Course } from './model/course';
 import { CourseCardComponent } from './course-card/course-card.component';
@@ -9,19 +9,9 @@ import { CourseCardComponent } from './course-card/course-card.component';
     styleUrls: ['./app.component.css'],
     standalone: false
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
 
     courses = COURSES; //it contains the list of all courses
-
-    startDate = new Date(2000, 0, 1)
-
-    title = COURSES[0].description;
-
-    price = 9.99;
-
-    rate = 0.67
-
-    course = COURSES[0];
 
     // viewchild lesson
     @ViewChild('cardRef1', { read: ElementRef }) // instead of the componente name "CourseCardComponent", we can pass a template reference such as 'cardRef1'
@@ -31,15 +21,28 @@ export class AppComponent {
     card2: CourseCardComponent;
 
     // querying plain html elements
-    @ViewChild('container')
-    containerDiv: ElementRef;
+    @ViewChild('courseImage') //it's not possible since the 'courseImage' is inside the child component "ng-container"
+    courseImage: ElementRef;
+
+    // constructor
+    constructor(){
+
+    }
+
+    // the earliest possible moment where all the references pupulated by ViewChild are available, and it is called by the framework itself 
+    ngAfterViewInit(): void {
+        console.log("courseImage:", this.courseImage) // here is renderized, so it's available to access
+
+        this.courses[0].description = 'test';
+    }
 
     onCourseSelected(course:Course){
-        console.log("containerDiv", this.card1) //instead of console.log each element we can take the whole container
+        
     }
     // tracking function  
     trackCourse(index: number, course:Course){
         return course.id // returns a unique identifier gor each object
     }
+
 
 }
