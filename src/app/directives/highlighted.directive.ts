2396ab1ab59cd594cf076aa044e7a0d728627f48
david-vargas98 <edgar981125@live.com.mx>
2025-06-30 14:19:16 -0600

    feat: host binding.
    We make the 'highlighted' binding variable for data binding and we send the 'true' value to the component.
    
    After, an @Input decorator receives this 'highlighted' variable value associated, by default the 'isHighLighted' associated variable has a 'false' default value, but we've sent a 'true' one, so it's 'true'.
    
    Finally, we make use of the @HostBinding to assign to the component's class the 'highlighted' class, but this will only happen if the cssClasses() method returns true, an here is where we get use of the 'isHighLighted' variable that was set using the data binding.

--- src/app/directives/highlighted.directive.ts
+++ src/app/directives/highlighted.directive.ts
@@ -1,4 +1,4 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[highlighted]',
@@ -6,10 +6,18 @@ import { Directive } from '@angular/core';
 })
 export class HighlightedDirective {
 
+  @Input('highlighted')
+  isHighLighted = false
+
   constructor() {
     
     console.log("Directive created.")
    
+  }
+
+  @HostBinding('class.highlighted') // we'll modify the className property, and we use it as a getter method as we do with Js
+  get cssClasses(){
+    return this.isHighLighted;
   }
 
 }
