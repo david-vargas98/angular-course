680e4c208a8287369c6f3fad3dd5cbce9b286cbc
david-vargas98 <edgar981125@live.com.mx>
2025-07-02 18:50:54 -0600

    feat: custom service and data modifications with PUT.
    We've made an event binding using 'courseChanged' which executes the save() function along with an event.
    
    The component got injected with 'CoursesService' and the save() functions gets executed, and using the injected dependency, calls the 'courses.service' custom service. Also it stays susbscribed to wait for the result from the service.
    
    Then, the saveCourse() function gets executed, initializes the headers and finally, performs the PUT request. The resul will be pass to the subscriber.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,6 @@ import {Course} from './model/course';
 import {CourseCardComponent} from './course-card/course-card.component';
 import {HighlightedDirective} from './directives/highlighted.directive';
 import {Observable} from 'rxjs';
-import { HttpClient, HttpParams } from '@angular/common/http';
 import { CoursesService } from './services/courses.service';
 
 @Component({
@@ -27,6 +26,12 @@ export class AppComponent implements OnInit {
     
   }
 
-
+  save(course: Course){
+      
+    this.coursesService.saveCourse(course)
+    .subscribe(
+      () => console.log("Course saved!")
+    );
+  }
 
 }
