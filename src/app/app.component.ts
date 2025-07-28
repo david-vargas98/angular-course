f5bf5712095bfa272353e39297a9dd2308dfbc00
david-vargas98 <edgar981125@live.com.mx>
2025-07-27 20:02:04 -0600

    refactor: services - Standard DI (no manually).
    We changed the manual injection for the Direct DI, this means less code and angular manages all of the logic.
    
    Useful if we don't need to modify the service whule being injected.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,22 +7,12 @@ import {Observable} from 'rxjs';
 import { CoursesService } from './services/courses.service';
 import { HttpClient } from '@angular/common/http';
 
-function coursesServiceProvider(http: HttpClient): CoursesService {
-  return new CoursesService(http);
-}
-
-export const COURSES_SERVICE = new InjectionToken<CoursesService>('COURSES_SERVICE');
-
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
     providers: [ 
-      {
-        provide: COURSES_SERVICE, 
-        useFactory: coursesServiceProvider,
-        deps: [HttpClient]
-      } 
+      CoursesService
     ],
     standalone: false
 })
@@ -30,7 +20,7 @@ export class AppComponent implements OnInit {
 
   courses$ : Observable<Course[]>;
 
-  constructor(@Inject(COURSES_SERVICE) private coursesService: CoursesService) {
+  constructor(private coursesService: CoursesService) {
     
   }
 
