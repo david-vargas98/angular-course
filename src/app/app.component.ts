25e81ea03052fc9f4bbb2fbb136824f966503cbc
david-vargas98 <edgar981125@live.com.mx>
2025-07-28 14:38:08 -0600

    feat: Use of 'Onpush' change detection with observables.
    Making use of Onpush by using observables, along with the 'async' pipe.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, ElementRef, Inject, InjectionToken, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
+import {AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, Inject, InjectionToken, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
 import {COURSES} from '../db-data';
 import {Course} from './model/course';
 import {CourseCardComponent} from './course-card/course-card.component';
@@ -12,11 +12,10 @@ import { APP_CONFIG, AppConfig, CONFIG_TOKEN } from './config';
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    standalone: false
+    standalone: false,
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent implements OnInit {
-
-  courses = COURSES;
 
   courses$ : Observable<Course[]>;
 
@@ -26,19 +25,12 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {   
-        
+    this.courses$ = this.coursesService.loadCourses();
   }
 
   onEditCourse(){
 
-    const course = this.courses[0]; // we create a "course", and we get the "courses[0]" object
-
-    const newCourse: any = {...course} // copy of "course" using spread operator: allow us to create a new reference, this is needed 
-                                       // since "OnPush" doesn't detect the changes if we don't change the obj reference
-
-    newCourse.description = 'New Value'; // we assign the new value to the new reference
-
-    this.courses[0] = newCourse; // we assing the new reference object to the original object
+    
 
   }
 
