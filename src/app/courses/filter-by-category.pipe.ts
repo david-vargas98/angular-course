055bbf94c3e34dfd8b334a9900a7297e45405abc
david-vargas98 <edgar981125@live.com.mx>
2025-07-30 00:32:05 -0600

    feat: impure pipes.
    Working with impure pipes, which are called every change detection cycle, i.e. maybe when a property values has changed.

--- src/app/courses/filter-by-category.pipe.ts
+++ src/app/courses/filter-by-category.pipe.ts
@@ -3,9 +3,17 @@ import { Course } from "../model/course";
 
 @Pipe({
     name: 'filterByCategory',
-    standalone: false
+    standalone: false,
+    pure: false // if we want to make this pipe get executed for example, when a property value changes, just add pure property
+                // so this way, the inpure pipe is going to get called in every angular change detection cycle  
 })
 export class FilterByCategoryPipe implements PipeTransform {
+
+    // logic paste in here needs to be very lightweight in order to prevent any performance issue if possible, it's always better 
+    // to make our pipes PURE and only resort to impure pipes if we absolutely need to.
+    
+    //The problem with impure pipes is that if we do here some sort of expensive computation that might cause the whole user 
+    // experience to degrade as the user interface become very slow
     
     transform(courses: Course[], category: string) {
 
