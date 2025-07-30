0189316247af16b23e24976946ec5cd58bef328a
david-vargas98 <edgar981125@live.com.mx>
2025-07-30 16:31:50 -0600

    feat: main.ts - Entry point.
    We got rid of 'app.module' in order to convert the module application into a completely standalone app, by using 'bootstrapApplication' and deleting 'app.module'.
    
    You can use 'ng generate @angular/core:standalone' for automating the process of migrating from module to standalone.

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,47 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 if (environment.production) {
   enableProdMode();
 }
 
+// Bootstraping in angular: Here we tell to angular "Hey! This is my root component, you need to load it first!"
+
+// This is the traditional approach ranging from angular 2 to angular 14, here we define everything in the AppModule:
+// Components, Pipes, Services, Root component (bootstrap: [AppComponent]) 
+// angular takes the "AppModule", analyzes and then loads the root component that we've indicated 
+
+// Modules based:
+// - bootstrapModule(), we pass the "AppModule":
+
 // platformBrowserDynamic().bootstrapModule(AppModule)
 //   .catch(err => console.log(err));
 
-// We need to use standalone app without NgModule to run the standalone root component, not the module, so,
-// we need to use "bootstrapApplication(AppComponent)" and convert "AppComponent" into standalone, without modules  
+//This is the new approach, that needs to be used for standalone applications without getting use of NgModule, so that, to run the 
+// standalone root component, not the module, hence, we need to use "bootstrapApplication(AppComponent)" and convert 
+// "AppComponent" into standalone (for this case), without modules
+
+// Here "AppComponent" becomes completely indipendent (standalone: true)
+// all what we need is imported and configured directly into the component
+// it's lighter, more flexible and more modern, buuuuut, WE TAKE CARE of importing everythin: directives such as NgIf, pipes, 
+// services, etc., because there is no longer a module which take care of it for you.
+
+// Standalone:
+// - bootstrapApplication(), we pass the "AppComponent":
 bootstrapApplication(AppComponent, {
   providers: [
-    provideHttpClient() // this enables HttpClient for injection, is mandatory if we run from "bootstrapApplication"
+    importProvidersFrom(BrowserModule), // DOM functionalities (ngIf, ngFor, bindings, etc); Directives such as NgIf, NgForOf
+    provideAnimations(), // Support animations for angular, necessary for Angular Material or directives such as [@fade]
+    provideHttpClient(withInterceptorsFromDi()) // this enables HttpClient for injection, is mandatory if we run from "bootstrapApplication"
+                                              // using withInterceptorsFromDi() enables interceptors such as 
+                                              // @Injectable({ providedIn: 'root' }), it's the recommended if we came from a modular app
+                                               
   ]
 })
   .catch(err => console.error(err));
