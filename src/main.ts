98d9d9034b4a64e2381f9987c89329d01ca1b5a7
david-vargas98 <edgar981125@live.com.mx>
2025-07-30 15:16:47 -0600

    feat: standalone - app entry point.
    The entry point of the application was changed to be compatible with the standalone components approach, also provideHttpClient() was added to handle http requests in the whole application.

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,22 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { provideHttpClient } from '@angular/common/http';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+// platformBrowserDynamic().bootstrapModule(AppModule)
+//   .catch(err => console.log(err));
+
+// We need to use standalone app without NgModule to run the standalone root component, not the module, so,
+// we need to use "bootstrapApplication(AppComponent)" and convert "AppComponent" into standalone, without modules  
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient() // this enables HttpClient for injection, is mandatory if we run from "bootstrapApplication"
+  ]
+})
+  .catch(err => console.error(err));
