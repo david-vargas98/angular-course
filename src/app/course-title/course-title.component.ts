4be892402b7c7959e3d572cedaf36014efdd6b2e
david-vargas98 <edgar981125@live.com.mx>
2025-07-30 13:34:21 -0600

    feat: angular elements in detail.
    Creation of a component which serves as an 'angular component', we added the property title with the @Input decorator and showed it within the html template, as well as we've added a style for the element.

---
+++ src/app/course-title/course-title.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'course-title',
+  imports: [],
+  templateUrl: './course-title.component.html',
+  styleUrl: './course-title.component.css'
+})
+export class CourseTitleComponent {
+
+  @Input()
+  title:string;
+
+}
